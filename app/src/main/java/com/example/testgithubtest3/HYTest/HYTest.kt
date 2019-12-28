6bfbb01659714dafa1d895b3c04b74796455c36d
threwhockyu <thy966@hotmail.com>
2019-12-28 20:04:19 +0800

    Testing branches

--- app/src/main/java/com/example/testgithubtest3/HYTest/HYTest.kt
+++ app/src/main/java/com/example/testgithubtest3/HYTest/HYTest.kt
@@ -9,5 +9,6 @@ class HYTest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hytest)
+        //Testing branches 
     }
 }
