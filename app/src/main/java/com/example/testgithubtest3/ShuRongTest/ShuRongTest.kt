8b37b07e4c7bb2e8792e303482bfff044f0bb66f
Lumis08 <limbin234@gmail.com>
2019-12-28 12:40:01 +0800

    Shu Rong Test 1

---
+++ app/src/main/java/com/example/testgithubtest3/ShuRongTest/ShuRongTest.kt
@@ -0,0 +1,13 @@
+package com.example.testgithubtest3.ShuRongTest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testgithubtest3.R
+
+class ShuRongTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shu_rong_test)
+    }
+}
